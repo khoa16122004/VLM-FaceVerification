7fc0bd5e140307f9f2733b1195a43a9c1e51cbbb
khoa16122004 <khoatr.work@gmail.com>
2025-06-09 12:32:46 +0700

    Create [script]unzip.py

---
+++ [script]unzip.py
@@ -0,0 +1,12 @@
+import zipfile
+import os
+
+def zip_folder(folder_path, output_zip_path):
+    with zipfile.ZipFile(output_zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, folder_path)
+                zipf.write(file_path, arcname)
+
+zip_folder('lfw.zip', 'lfw')
