31308536c4a7f4f3ea6ba81bd28b5ae01cde0193
khoa16122004 <khoatr.work@gmail.com>
2025-06-11 12:22:26 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1 # So GPU can dung
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+python [script]eval_bench.py --start_index 1078 --img_dir lfw/lfw/images --pair_path lfw/lfw/pairs.txt
