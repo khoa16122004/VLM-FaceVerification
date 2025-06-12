f5b5e5a67b5e08ea14a2b84803ef4d6467bad6c0
khoa16122004 <khoatr.work@gmail.com>
2025-06-12 14:24:19 +0700

    update

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 #SBATCH -N 1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-python "[script]eval_bench.py" --start_index 2280 --pretrained_lvlm llava-next-interleave-qwen-7b --img_dir lfw/lfw/images --pair_path lfw/lfw/pairs.txt
+ python "[script]eval_bench.py" --img_dir lfw/images --pair_path lfw/1000_pair.txt --pretrained_lvlm llava-onevision-qwen2-7b-ov --model_name_lvlm llava_qwen --llm_model Llama-7b --num_samples 9
