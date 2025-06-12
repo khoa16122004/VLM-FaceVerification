17190decad8520d057fc618110ee6c42ede767c4
khoa16122004 <khoatr.work@gmail.com>
2025-06-13 00:30:57 +0700

    update

--- run_vast.sh
+++ run_vast.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-#SBATCH --gres=gpu:1 # So GPU can dung
-#SBATCH -N 1
-#SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1
 python "[script]eval_bench.py" --img_dir lfw/images --pair_path lfw/500_pair.txt --pretrained_lvlm llava-onevision-qwen2-7b-ov --model_name_lvlm llava_qwen --llm_model Llama-7b --num_samples 5
 python "[script]eval_bench.py" --img_dir lfw/images --pair_path lfw/500_pair.txt --pretrained_lvlm llava-onevision-qwen2-7b-ov --model_name_lvlm llava_qwen --llm_model Llama-7b --num_samples 3
